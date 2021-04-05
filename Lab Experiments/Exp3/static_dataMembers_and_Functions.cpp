e4125aa6ad725de473eaa2105403c276b44f3101
Mohak Sharma <mohak327@gmail.com>
2021-04-05 10:53:44 +0530

    static data member n function

---
+++ Lab Experiments/Exp3/static_dataMembers_and_Functions.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+class X {
+    int codeno;
+    float price;
+    // Static data member
+    static int count;
+
+public:
+    void getval(int i, float j){
+        codeno = i;
+        price = j;
+        ++count;
+    }
+
+    void display(void){
+        cout <<" Code no :"<< code no << " \t";
+        cout <<" Price :"<< price << " \n" ;
+    }
+
+    // Static member function
+    static void dispcount(void){
+        cout <<" count = "<< count <<" \n";
+    }
+};
+
+int X:: count = 0;
+
+int main (){
+    X Ob1, Ob2 ;
+    Ob1.getval(101, 25.12);
+    Ob2.getval(102, 38.19);
+
+    X :: dispcount();
+    X Ob3;
+    Ob3.getval(103, 49.00);
+    X :: dispcount();
+
+    Obl.display();
+    Ob2.display();
+    Ob3.display();
+
+    cout<<"Made by: Mohak Sharma, 12-CSEA-19"
+    return 0;
+}
