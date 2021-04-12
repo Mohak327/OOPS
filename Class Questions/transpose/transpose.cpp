90dad9d1cde7bb61c5d5d086f3df1db6d867c86e
Mohak Sharma <mohak327@gmail.com>
2021-04-12 11:20:10 +0530

    output error in transpose

---
+++ Class Questions/transpose/transpose.cpp
@@ -0,0 +1,60 @@
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+
+void printMatrix(int **arr,int row,int col){
+	for(int i=0; i<row; i++) {
+		for(int j=0; j<col; j++) {
+			cout<< arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int** inputMatrix(int row,int col){
+	int** A = new int*[row];
+	for(int i=0; i<row; i++){
+		A[i] = new int[col];
+		for(int j=0; j<col; j++){
+			cin >> A[i][j];
+		}
+	}
+	return A;
+}
+
+void transposeMatrix(int** A, int row, int col){
+		int **transpose = new int*[row];
+		for(int i=0; i<row; i++){
+			transpose[i] = new int[col];
+			for(int j=0; j<col; j++){
+				transpose[i][j] = A[j][i];
+			}
+		printMatrix(transpose,row,col);
+	}
+}
+
+int main(){
+	int row,col;
+
+	cout<<"Enter number of rows of matrix: ";
+	cin>>row;
+	cout<<"Enter number of columns of matrix: ";
+	cin>>col;
+
+	cout << "Enter the elements of matrix: ";
+	int**A = inputMatrix(row,col);
+
+	cout<<"Matrix A is \n";
+	printMatrix(A,row,col);
+
+    cout<<"Transpose of matrix is \n";
+	transposeMatrix(A,row,col);
+
+	return 0;
+}
+
+
+
+
