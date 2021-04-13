a7454de7b692750803a63db2dfcf572810f83c65
jindal2209 <shubham7811@yahoo.com>
2021-04-13 10:55:32 +0530

    Exp4 (b)

---
+++ Lab Experiments/Exp4/a.cpp
@@ -0,0 +1,11 @@
+// Write a program to implement the concept of pass by value using TIME class. 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int Time;
+	cin>>Time;
+	
+	return 0;
+}
