4091e1635cc3d6aabe85f76c854c05b086c1ae0d
jindal2209 <shubham7811@yahoo.com>
2021-04-20 11:01:15 +0530

    Exp 6b

---
+++ Lab Experiments/Exp6/b.cpp
@@ -0,0 +1,40 @@
+//Write a program to overload binary(+) operator
+#include<iostream>
+using namespace std;
+  
+class Complex {
+    int real, imag;
+	public:
+	    Complex(int r = 0, int i =0){
+			real = r;   imag = i;
+		}
+	 
+	    Complex operator + (Complex const &obj) {
+	         Complex res;
+	         res.real = real + obj.real;
+	         res.imag = imag + obj.imag;
+	         return res;
+	    }
+	    void printNumber(){
+			cout<<"Real part: "<<this->real<<endl;
+			cout<<"Imaginary part: "<<this->imag<<endl;
+		}
+};
+  
+int main(){
+    Complex c1(10, 5), c2(2, 4);
+    cout<<"c1"<<endl;
+    c1.printNumber();
+    cout<<endl;
+    
+    cout<<"c3"<<endl;
+    c2.printNumber();
+    cout<<endl;
+    
+	Complex c3 = c1 + c2;
+    cout<<"c3"<<endl;
+	c3.printNumber();
+    
+    
+    cout<<endl<<endl<<"Shubham Jindal\nCSE-A\n40";
+}
